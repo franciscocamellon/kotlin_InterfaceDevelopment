b3b1d87a7df72831708a4c9435c771b697f4604c
Francisco Alves Camello Neto <franciscocamellon@gmail.com>
2021-11-28 17:21:19 -0300

    Initial code

---
+++ tp03/app/src/main/java/camell/oncase/interfacedevelopmenttp03/ConstraintLayout.kt
@@ -0,0 +1,11 @@
+package camell.oncase.interfacedevelopmenttp03
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConstraintLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint_layout)
+    }
+}
